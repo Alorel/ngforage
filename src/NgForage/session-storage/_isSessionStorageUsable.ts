e7ad6096df9df4e79c72c41fdba00d8df57d2620
Patryk Panek <patrykpanek57@gmail.com>
2018-04-11 14:20:54 +0200

    ReferenceError: sessionStorage is not defined - one line rewrite

--- src/NgForage/session-storage/_isSessionStorageUsable.ts
+++ src/NgForage/session-storage/_isSessionStorageUsable.ts
@@ -2,13 +2,5 @@ import {checkIfSessionStorageThrows} from './checkIfSessionStorageThrows';
 
 /** @internal */
 export function _isSessionStorageUsable() {
-    if (typeof sessionStorage !== 'undefined') {
-        if( !checkIfSessionStorageThrows() || sessionStorage.length > 0) {
-            return true;
-        } else {
-            return false;
-        }
-    } else {
-        return false
-    }
+    return typeof sessionStorage !== 'undefined' && (!checkIfSessionStorageThrows() || sessionStorage.length > 0);
 }
