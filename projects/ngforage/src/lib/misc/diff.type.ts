191ac6616b588a4cd79a1ae99328f9226dea5953
Arturas <a.molcanovas@gmail.com>
2018-10-22 14:47:12 +0100

    refactor: Remove diff type in favour of the built-in Exclude type

--- projects/ngforage/src/lib/misc/diff.type.ts
+++
@@ -1,3 +0,0 @@
-//tslint:disable:max-line-length
-/** @see http://ideasintosoftware.com/typescript-advanced-tricks/ */
-export type Diff<T extends PropertyKey, U extends PropertyKey> = ({ [P in T]: P } & { [P in U]: never } & { [x: string]: never })[T];
--- projects/ngforage/src/lib/misc/omit.type.ts
+++ projects/ngforage/src/lib/misc/omit.type.ts
@@ -1,7 +1,1 @@
-import {Diff} from './diff.type';
-
-/**
- * Instead of picking separate properties, omit them
- * @see http://ideasintosoftware.com/typescript-advanced-tricks/
- */
-export type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
