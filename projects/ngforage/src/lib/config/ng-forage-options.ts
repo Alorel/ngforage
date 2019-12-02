ff4ec1d223e7d24684b0fb512a8a87bcd93095bc
Arturas Molcanovas <a.molcanovas@gmail.com>
2019-12-02 19:53:51 +0000

    chore: Removed the Omit type as Typescript now bundles its own.
    BREAKING CHANGE: Removed the Omit type as Typescript now bundles its own. ng update will handle this for you

--- projects/ngforage/src/lib/config/ng-forage-options.ts
+++ projects/ngforage/src/lib/config/ng-forage-options.ts
@@ -1,6 +1,5 @@
 import 'localforage';
 import {DriverType} from '../misc/driver-type.type';
-import {Omit} from '../misc/omit.type';
 
 /** Localforage options with ngforage formatting */
 export type FormattedLocalForageOptions = Omit<LocalForageOptions, 'driver'> & {
