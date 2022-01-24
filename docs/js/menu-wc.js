50ad2f58deaef86012df1f9ea13cbb650cc692e4
Alorel <a.molcanovas@gmail.com>
2022-01-24 16:52:23 +0000

    Deploying to gh-pages from @ Alorel/ngforage@8441de47f2c019575bdeade9cbf99b032206dd28 🚀

---
+++ docs/js/menu-wc.js
@@ -0,0 +1,124 @@
+'use strict';
+
+customElements.define('compodoc-menu', class extends HTMLElement {
+    constructor() {
+        super();
+        this.isNormalMode = this.getAttribute('mode') === 'normal';
+    }
+
+    connectedCallback() {
+        this.render(this.isNormalMode);
+    }
+
+    render(isNormalMode) {
+        let tp = lithtml.html(`
+        <nav>
+            <ul class="list">
+                <li class="title">
+                    <a href="index.html" data-type="index-link">NgForage API docs</a>
+                </li>
+
+                <li class="divider"></li>
+                ${ isNormalMode ? `<div id="book-search-input" role="search"><input type="text" placeholder="Type to search"></div>` : '' }
+                <li class="chapter">
+                    <a data-type="chapter-link" href="index.html"><span class="icon ion-ios-home"></span>Getting started</a>
+                    <ul class="links">
+                        <li class="link">
+                            <a href="overview.html" data-type="chapter-link">
+                                <span class="icon ion-ios-keypad"></span>Overview
+                            </a>
+                        </li>
+                        <li class="link">
+                            <a href="index.html" data-type="chapter-link">
+                                <span class="icon ion-ios-paper"></span>README
+                            </a>
+                        </li>
+                        <li class="link">
+                            <a href="changelog.html"  data-type="chapter-link">
+                                <span class="icon ion-ios-paper"></span>CHANGELOG
+                            </a>
+                        </li>
+                        <li class="link">
+                            <a href="license.html"  data-type="chapter-link">
+                                <span class="icon ion-ios-paper"></span>LICENSE
+                            </a>
+                        </li>
+                                <li class="link">
+                                    <a href="dependencies.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-list"></span>Dependencies
+                                    </a>
+                                </li>
+                    </ul>
+                </li>
+                        <li class="chapter">
+                            <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#injectables-links"' :
+                                'data-target="#xs-injectables-links"' }>
+                                <span class="icon ion-md-arrow-round-down"></span>
+                                <span>Injectables</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                            <ul class="links collapse " ${ isNormalMode ? 'id="injectables-links"' : 'id="xs-injectables-links"' }>
+                                <li class="link">
+                                    <a href="injectables/BaseConfigurableImpl.html" data-type="entity-link" >BaseConfigurableImpl</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/DedicatedInstanceFactory.html" data-type="entity-link" >DedicatedInstanceFactory</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/InstanceFactory.html" data-type="entity-link" >InstanceFactory</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/NgForage.html" data-type="entity-link" >NgForage</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/NgForageCache.html" data-type="entity-link" >NgForageCache</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/NgForageConfig.html" data-type="entity-link" >NgForageConfig</a>
+                                </li>
+                            </ul>
+                        </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#interfaces-links"' :
+                            'data-target="#xs-interfaces-links"' }>
+                            <span class="icon ion-md-information-circle-outline"></span>
+                            <span>Interfaces</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? ' id="interfaces-links"' : 'id="xs-interfaces-links"' }>
+                            <li class="link">
+                                <a href="interfaces/BaseConfigurable.html" data-type="entity-link" >BaseConfigurable</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/CacheConfigurable.html" data-type="entity-link" >CacheConfigurable</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/CachedItem.html" data-type="entity-link" >CachedItem</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#miscellaneous-links"'
+                            : 'data-target="#xs-miscellaneous-links"' }>
+                            <span class="icon ion-ios-cube"></span>
+                            <span>Miscellaneous</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="miscellaneous-links"' : 'id="xs-miscellaneous-links"' }>
+                            <li class="link">
+                                <a href="miscellaneous/enumerations.html" data-type="entity-link">Enums</a>
+                            </li>
+                            <li class="link">
+                                <a href="miscellaneous/typealiases.html" data-type="entity-link">Type aliases</a>
+                            </li>
+                            <li class="link">
+                                <a href="miscellaneous/variables.html" data-type="entity-link">Variables</a>
+                            </li>
+                        </ul>
+                    </li>
+            </ul>
+        </nav>
+        `);
+        this.innerHTML = tp.strings;
+    }
+});
--- docs/js/menu.js
+++ docs/js/menu.js
@@ -1,17 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var menuCollapsed = false,
         mobileMenu = document.getElementById('mobile-menu');
 
     var localContextInUrl = '';
-    
+
     if (COMPODOC_CURRENT_PAGE_CONTEXT !== '') {
-        localContextInUrl = localContextInUrl;
         switch (COMPODOC_CURRENT_PAGE_CONTEXT) {
             case 'additional-page':
-                localContextInUrl = 'additional-documentation'
+                localContextInUrl = 'additional-documentation';
                 break;
             case 'class':
-                localContextInUrl = 'classes'
+                localContextInUrl = 'classes';
                 break;
             case 'miscellaneous-functions':
             case 'miscellaneous-variables':
@@ -24,13 +23,13 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function hasClass(el, cls) {
-        return el.className && new RegExp("(\\s|^)" + cls + "(\\s|$)").test(el.className);
+        return el.className && new RegExp('(\\s|^)' + cls + '(\\s|$)').test(el.className);
     }
 
-    var processLink = function(link, url) {
+    var processLink = function (link, url) {
         if (url.charAt(0) !== '.') {
-            let prefix = '';
-            switch(COMPODOC_CURRENT_PAGE_DEPTH) {
+            var prefix = '';
+            switch (COMPODOC_CURRENT_PAGE_DEPTH) {
                 case 5:
                     prefix = '../../../../../';
                     break;
@@ -52,22 +51,26 @@ document.addEventListener('DOMContentLoaded', function() {
             }
             link.setAttribute('href', prefix + url);
         }
-    }
+    };
 
-    var processMenuLinks = function(links, dontAddClass) {
+    var processMenuLinks = function (links, dontAddClass) {
         for (var i = 0; i < links.length; i++) {
             var link = links[i];
             var linkHref = link.getAttribute('href');
-            if (linkHref.toLowerCase().indexOf(COMPODOC_CURRENT_PAGE_URL.toLowerCase()) !== -1 
-                && link.innerHTML.indexOf('Getting started') == -1 
-                && !dontAddClass
-                && linkHref.toLowerCase().indexOf(localContextInUrl.toLowerCase()) !== -1 ) {
-                link.classList.add('active');
+            if (linkHref) {
+                var linkHrefFile = linkHref.substr(linkHref.lastIndexOf('/') + 1, linkHref.length);
+                if (
+                    linkHrefFile.toLowerCase() === COMPODOC_CURRENT_PAGE_URL.toLowerCase() &&
+                    link.innerHTML.indexOf('Getting started') == -1 &&
+                    !dontAddClass &&
+                    linkHref.toLowerCase().indexOf(localContextInUrl.toLowerCase()) !== -1
+                ) {
+                    link.classList.add('active');
+                }
+                processLink(link, linkHref);
             }
-            processLink(link, linkHref);
-
         }
-    }
+    };
     var chapterLinks = document.querySelectorAll('[data-type="chapter-link"]');
     processMenuLinks(chapterLinks);
     var entityLinks = document.querySelectorAll('[data-type="entity-link"]');
@@ -75,14 +78,19 @@ document.addEventListener('DOMContentLoaded', function() {
     var indexLinks = document.querySelectorAll('[data-type="index-link"]');
     processMenuLinks(indexLinks, true);
     var entityLogos = document.querySelectorAll('[data-type="compodoc-logo"]');
-    var processLogos = function(entityLogo) {
+    var processLogos = function (entityLogo) {
         for (var i = 0; i < entityLogos.length; i++) {
             var entityLogo = entityLogos[i];
             if (entityLogo) {
                 var url = entityLogo.getAttribute('data-src');
+                // Dark mode + logo
+                let isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+                if (isDarkMode) {
+                    url = 'images/compodoc-vectorise-inverted.png';
+                }
                 if (url.charAt(0) !== '.') {
-                    let prefix = '';
-                    switch(COMPODOC_CURRENT_PAGE_DEPTH) {
+                    var prefix = '';
+                    switch (COMPODOC_CURRENT_PAGE_DEPTH) {
                         case 5:
                             prefix = '../../../../../';
                             break;
@@ -100,17 +108,17 @@ document.addEventListener('DOMContentLoaded', function() {
                             break;
                         case 0:
                             prefix = './';
-                            break
+                            break;
                     }
                     entityLogo.src = prefix + url;
                 }
             }
         }
-    }
+    };
     processLogos(entityLogos);
 
-    setTimeout(function() {
-        document.getElementById('btn-menu').addEventListener('click', function() {
+    setTimeout(function () {
+        document.getElementById('btn-menu').addEventListener('click', function () {
             if (menuCollapsed) {
                 mobileMenu.style.display = 'none';
             } else {
@@ -120,11 +128,22 @@ document.addEventListener('DOMContentLoaded', function() {
             menuCollapsed = !menuCollapsed;
         });
 
+        /**
+         * Native bootstrap doesn't wait DOMContentLoaded event to start his job, re do it here
+         */
+        var Collapses = document.querySelectorAll('[data-toggle="collapse"]');
+        for (var o = 0, cll = Collapses.length; o < cll; o++) {
+            var collapse = Collapses[o],
+                options = {};
+            options.duration = collapse.getAttribute('data-duration');
+            var c = new Collapse(collapse, options);
+        }
+
         // collapse menu
         var classnameMenuToggler = document.getElementsByClassName('menu-toggler'),
-            faAngleUpClass = 'fa-angle-up',
-            faAngleDownClass = 'fa-angle-down',
-            toggleItemMenu = function(e) {
+            faAngleUpClass = 'ion-ios-arrow-up',
+            faAngleDownClass = 'ion-ios-arrow-down',
+            toggleItemMenu = function (e) {
                 var element = $(e.target),
                     parent = element[0].parentNode,
                     parentLink,
@@ -143,7 +162,7 @@ document.addEventListener('DOMContentLoaded', function() {
                         elementIconChild = parent.getElementsByClassName(faAngleDownClass)[0];
                     }
                     if (elementIconChild) {
-                        elementIconChild = $(elementIconChild)
+                        elementIconChild = $(elementIconChild);
                         if (elementIconChild.hasClass(faAngleUpClass)) {
                             elementIconChild.addClass(faAngleDownClass);
                             elementIconChild.removeClass(faAngleUpClass);
@@ -167,7 +186,7 @@ document.addEventListener('DOMContentLoaded', function() {
             activeMenuClass,
             activeLink;
 
-        for (i; i<len; i++) {
+        for (i; i < len; i++) {
             if (getComputedStyle(menus[i]).display != 'none') {
                 activeMenu = menus[i];
                 activeMenuClass = activeMenu.getAttribute('class').split(' ')[0];
@@ -189,7 +208,8 @@ document.addEventListener('DOMContentLoaded', function() {
                             parentChapterMenu = parentUl.parentNode;
                             if (parentChapterMenu) {
                                 var toggler = parentChapterMenu.querySelector('.menu-toggler'),
-                                    elementIconChild = toggler.getElementsByClassName(faAngleUpClass)[0];
+                                    elementIconChild =
+                                        toggler.getElementsByClassName(faAngleUpClass)[0];
                                 if (toggler && !elementIconChild) {
                                     toggler.click();
                                 }
@@ -199,23 +219,100 @@ document.addEventListener('DOMContentLoaded', function() {
                     if (linkContext && linkContext === 'sub-entity') {
                         // Toggle also the master parent menu
                         var linkContextId = activeLink.getAttribute('data-context-id');
-                        var toggler = activeMenu.querySelector('.chapter.' + linkContextId + ' a .menu-toggler');
+                        var toggler = activeMenu.querySelector(
+                            '.chapter.' + linkContextId + ' a .menu-toggler'
+                        );
                         if (toggler) {
                             toggler.click();
                         }
+                        if (linkContextId === 'additional') {
+                            var mainToggler = activeMenu.querySelector(
+                                '.chapter.' + linkContextId + ' div.menu-toggler'
+                            );
+                            if (mainToggler) {
+                                mainToggler.click();
+                            }
+                        }
                     }
                 } else if (linkType === 'chapter-link') {
+                    var linkContextId = activeLink.getAttribute('data-context-id');
                     var toggler = activeLink.querySelector('.menu-toggler');
                     if (toggler) {
                         toggler.click();
                     }
+                    if (linkContextId === 'additional') {
+                        var mainToggler = activeMenu.querySelector(
+                            '.chapter.' + linkContextId + ' div.menu-toggler'
+                        );
+                        if (mainToggler) {
+                            mainToggler.click();
+                        }
+                    }
                 }
-                setTimeout(function() {
+                setTimeout(function () {
                     activeMenu.scrollTop = activeLink.offsetTop;
-                    if (activeLink.innerHTML.toLowerCase().indexOf('readme') != -1 || activeLink.innerHTML.toLowerCase().indexOf('overview') != -1) {
+                    if (
+                        activeLink.innerHTML.toLowerCase().indexOf('readme') != -1 ||
+                        activeLink.innerHTML.toLowerCase().indexOf('overview') != -1
+                    ) {
                         activeMenu.scrollTop = 0;
                     }
                 }, 300);
+            }
+        }
+        // Dark mode toggle button
+        var useDark = window.matchMedia('(prefers-color-scheme: dark)');
+        var darkModeState = useDark.matches;
+        var $darkModeToggleSwitchers = document.querySelectorAll('.dark-mode-switch input');
+        var $darkModeToggles = document.querySelectorAll('.dark-mode-switch');
+        var darkModeStateLocal = localStorage.getItem('compodoc_darkmode-state');
+
+        function checkToggle(check) {
+            for (var i = 0; i < $darkModeToggleSwitchers.length; i++) {
+                $darkModeToggleSwitchers[i].checked = check;
+            }
+        }
+
+        function toggleDarkMode(state) {
+            if (window.localStorage) {
+                localStorage.setItem('compodoc_darkmode-state', state);
+            }
+
+            checkToggle(state);
+
+            const hasClass = document.body.classList.contains('dark');
+
+            if (state) {
+                for (var i = 0; i < $darkModeToggles.length; i++) {
+                    $darkModeToggles[i].classList.add('dark');
+                }
+                if (!hasClass) {
+                    document.body.classList.add('dark');
+                }
+            } else {
+                for (var i = 0; i < $darkModeToggles.length; i++) {
+                    $darkModeToggles[i].classList.remove('dark');
+                }
+                if (hasClass) {
+                    document.body.classList.remove('dark');
+                }
+            }
+        }
+
+        useDark.addEventListener('change', function (evt) {
+            toggleDarkMode(evt.matches);
+        });
+        if (darkModeStateLocal) {
+            darkModeState = darkModeStateLocal === 'true';
+        }
+        toggleDarkMode(darkModeState);
+
+        if ($darkModeToggles.length > 0) {
+            for (var i = 0; i < $darkModeToggleSwitchers.length; i++) {
+                $darkModeToggleSwitchers[i].addEventListener('change', function (event) {
+                    darkModeState = !darkModeState;
+                    toggleDarkMode(darkModeState);
+                });
             }
         }
     }, 0);
