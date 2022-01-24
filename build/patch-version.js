9b8785a0cfd6d914294b3abf67d69436512c592b
Arturas <a.molcanovas@gmail.com>
2022-01-24 16:30:04 +0000

    build: Update build system

--- build/patch-version.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const mainVersion = require('../package.json').version;
-const targetPkgJsonPath = require.resolve('../dist/ngforage/package.json');
-const targetPkgJson = JSON.parse(fs.readFileSync(targetPkgJsonPath, 'utf8'));
-
-targetPkgJson.version = mainVersion;
-
-fs.writeFileSync(targetPkgJsonPath, JSON.stringify(targetPkgJson, null, 2));
