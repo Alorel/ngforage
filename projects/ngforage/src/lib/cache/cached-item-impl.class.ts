48cdab6b037e29880e72652584fbb27dffb2af7b
Arturas <a.molcanovas@gmail.com>
2022-11-13 15:24:23 +0000

    chore: remove lazy-get-decorator dependency

--- projects/ngforage/src/lib/cache/cached-item-impl.class.ts
+++ projects/ngforage/src/lib/cache/cached-item-impl.class.ts
@@ -1,4 +1,3 @@
-import {LazyGetter} from 'lazy-get-decorator';
 import type {CachedItem} from './cached-item';
 
 /** @internal */
@@ -6,23 +5,19 @@ export class CachedItemImpl<T> implements CachedItem<T> {
 
   public readonly expires: Date;
 
+  public readonly hasData: boolean;
+
   public constructor(public readonly data: T, expiryTime: number) {
     this.expires = new Date(typeof <any>expiryTime === 'number' ? expiryTime : 0);
+    this.hasData = data != null;
   }
 
-  @LazyGetter(false, false, Boolean)
   public get expired(): boolean {
     return this.expiresIn === 0;
   }
 
-  @LazyGetter(false, false, (v: any) => !v)
   public get expiresIn(): number {
     return Math.max(0, this.expires.getTime() - Date.now());
-  }
-
-  @LazyGetter()
-  public get hasData(): boolean {
-    return this.data != null;
   }
 
   public toJSON(): CachedItem<T> {
